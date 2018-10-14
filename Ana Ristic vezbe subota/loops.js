ab1d27a9b404e1ef88d6df4b1672da09d0cc7265
Nenad Bugarić <n.bugaric@gmail.com>
2018-10-14 22:58:58 +0200

    conditions and loops exe.

---
+++ Ana Ristic vezbe subota/loops.js
@@ -0,0 +1,11 @@
+// Write a for loop that will iterate from 0 to 15.
+// For each iteration, it will check if the current number is odd or even,
+// and display a message to the screen.
+
+for (var brojac = 0; brojac <= 15; brojac++) {
+  if (brojac % 2 === 0) {
+    console.log("the number is even");
+  } else {
+    console.log("the number is odd");
+  }
+}
